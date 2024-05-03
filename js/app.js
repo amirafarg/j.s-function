b43d88291362d841d0a7db885e60f261fc1871e6
amirafarag <amiraazam301@gmail.com>
2024-05-03 08:27:22 +0300

    added-concat

--- js/app.js
+++ js/app.js
@@ -41,6 +41,17 @@ console.log(multiplyArr ([2, 4, 6]));
 
 //4-return a string concatenation of the concatString and all elements of an array of strings.
 
+function concat(arr){
+   var result = "";
+
+   for (var index = 0; index < arr.length; index += 1) {
+    result += arr[index] + ", ";
+   }
+
+   return result + arr[arr.length - 1];
+}
+
+console.log(concat (['asmaa', 'amira', 'israa', 'mariam', 'ali']));
 
 
 
@@ -49,14 +60,14 @@ console.log(multiplyArr ([2, 4, 6]));
 
 //in-sec
 
-function getEvenNambersFrom1To99 () {
-    var results = [];
+// function getEvenNambersFrom1To99 () {
+//     var results = [];
 
-    for (var number = 1; number <= 99; number += 3 ) {
-        results.push(number);
-    }
+//     for (var number = 1; number <= 99; number += 3 ) {
+//         results.push(number);
+//     }
 
-    console.log(results);
+//     console.log(results);
 
-}
-getEvenNambersFrom1To99 ();
+// }
+// getEvenNambersFrom1To99 ();
